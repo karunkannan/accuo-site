103bb7a882df59997c209e7b01a5bd2017c2ca52
Vicmart1 <vdadfar1@gmail.com>
2017-03-23 19:26:40 -0400

    major update

---
+++ README.md
@@ -0,0 +1,10 @@
+# accuo-site
+
+[Live link (Usually a push or two behind)](https://vicmart1.github.io/accuo-site/)
+
+# To-do
+
+Mission page - Blurb + icon or two (homemade) - Top/Bottom layout  
+Problem page - Blurb + video - idk go crazy  
+Solution page - Blurb + CAD image - Left/Right layout  
+Team page - self-explanatory - also go crazy  
--- js/index.js
+++ js/index.js
@@ -50,11 +50,23 @@ $(document).ready(function() {
     var pane = parseInt(scroll/window.innerHeight);
     
     currentPane = pane;
+    
+    $(".article").each(function() {
+        $(this).css("height", $(this).css("height"));
+        $(this).children(".popout").children(".body").css("height", "auto");
+        $(this).children(".popout").children(".body").css("padding-bottom", "10px");
+    });
 });
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop() + (window.innerHeight/2);
-    var pane = parseInt(scroll/window.innerHeight);
+    var pane = 0;
+    
+    $(".pane").each(function() {
+        if (scroll > $(this).position().top) {
+            pane = $(".pane").index($(this));
+        }
+    });
     
     if (pane != currentPane) {
         $(".bubble-cont").children(".bubble").removeClass("big-bubble");
@@ -66,11 +78,53 @@ $(window).scroll(function (event) {
         clearTimeout(bubbleTimer);
         
         bubbleTimer = setTimeout(function() {
-            console.log("yo");
             $(".bubble-cont").children(".bubble-title").removeClass("bubble-title-show");
-        }, 5000);
+        }, 3000);
         currentPane = pane;
+        
+        if (currentPane == 5) {
+            $(".bubble").addClass("bubble-white");
+            $(".big-bubble").addClass("big-bubble-white");
+            $(".news-bubble-title").addClass("news-bubble-title-white");
+        } else {
+            $(".bubble").removeClass("bubble-white");
+            $(".big-bubble").removeClass("big-bubble-white");
+            $(".news-bubble-title").removeClass("news-bubble-title-white");
+        }
     }
+});
+
+$("input").focusin(function() {
+    if (!$(this).siblings("label").hasClass("input-label-active")) {
+        $(this).siblings("label").addClass("input-label-active");
+    }
+    $(this).addClass("input-active");
+});
+
+$("input").focusout(function() {
+    if ($(this).val().length == 0) {
+        $(this).siblings("label").removeClass("input-label-active");
+    }
+    $(this).removeClass("input-active");   
+    if ($(this).attr("id") == "email") {
+        $(".email-invalid").removeClass("email-invalid-active");
+    } else if ($(this).attr("id") == "name") {
+        $(".name-invalid").removeClass("email-invalid-active");
+    }
+});
+
+$("textarea").focusin(function() {
+    if (!$(this).siblings("label").hasClass("input-label-active")) {
+        $(this).siblings("label").addClass("input-label-active");
+    }
+    $(this).addClass("input-active");
+});
+
+$("textarea").focusout(function() {
+    if ($(this).val().length == 0) {
+        $(this).siblings("label").removeClass("input-label-active");
+    }
+    $(this).removeClass("input-active");
 });
 
 $("#home-click").click(function() {
@@ -113,4 +167,41 @@ $("#contact-click").click(function() {
     $("html, body").animate({
         scrollTop: $("#contact").offset().top
     });
+});
+
+$(".article").hover(function() {
+    var article_height = parseInt($(this).css("height"));
+    var popout_height = parseInt($(this).children(".popout").outerHeight());
+    var body_height = parseInt($(this).children(".popout").children(".body").css("height")) 
+    
+    var diff = popout_height - article_height;
+    
+    if (diff < 0) {
+        diff = 0;
+    } else {
+        $(this).addClass("article-active");
+    }
+    
+    
+    $(this).children(".popout").css("top", -1 * parseInt($(this).children(".popout").children(".body").outerHeight()) + diff);
+    
+    if (diff > 0) {
+        $(this).children(".popout").children(".body").css("height", body_height - diff);
+    }
+    
+    $(this).children(".popout").children(".exit").addClass("exit-active");
+}, function() {
+    removePopup($(this));
+});
+
+function removePopup(article) {
+    $(article).children(".popout").css("top", "0px");
+    $(article).children(".popout").animate({ scrollTop: 0 }, "fast");
+    $(article).removeClass("article-active");
+    $(article).children(".popout").children(".exit").removeClass("exit-active");
+}
+
+$(".exit").click(function() {
+    console.log("lol");
+    removePopup($(this).parent().parent());
 });
